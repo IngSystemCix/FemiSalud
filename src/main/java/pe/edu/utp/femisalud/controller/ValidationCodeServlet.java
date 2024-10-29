76527dbc348075890ca86a9493c0427a639f5266
IngSystemCix <juanromerocollazos@gmail.com>
2024-10-29 00:04:50 -0500

    update send email

---
+++ src/main/java/pe/edu/utp/femisalud/controller/ValidationCodeServlet.java
@@ -0,0 +1,8 @@
+package pe.edu.utp.femisalud.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/validate_code")
+public class ValidationCodeServlet extends HttpServlet {
+}
